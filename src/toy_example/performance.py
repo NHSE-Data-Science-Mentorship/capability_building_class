6d49e8a21d3dbc328a664e29cbdf96edc30286c2
John Mansir <john.m@faculty.ai>
2022-07-05 00:42:17 +0000

    Remove unnecessary f-string

--- src/toy_example/performance.py
+++ src/toy_example/performance.py
@@ -2,7 +2,7 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_sc
 
 
 def print_performance_metrics(y_true, y_pred):
-    print(f"True class balance:")
+    print("True class balance:")
     print(y_true.value_counts() / y_true.shape[0])
     print("")
     print(f"Accuracy score: {accuracy_score(y_true, y_pred)}")
